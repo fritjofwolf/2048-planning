a8d5e60ac74be3092e26f8a8840004b4109e4d9c
fritjof <fritjofwolf@gmail.com>
2015-05-16 22:49:40 +0200

    gameState parser part I

--- src/sel_example.py
+++ src/sel_example.py
@@ -1,26 +1,60 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import sys
+import numpy as np
+
+#a = '{"grid":{"size":4,"cells":[[null,null,null,null],[null,null,null,null],
+#[{"position":{"x":2,"y":0},"value":2},null,null,null],
+#[null,null,{"position":{"x":3,"y":2},"value":4},
+#{"position":{"x":3,"y":3},"value":4}]]},"score":8,"over":false,
+#"won":false,"keepPlaying":false}'
+
+b = '{"grid":{"size":4,"cells":[[null,null,null,null],[null,null,null,null],[{"position":{"x":2,"y":0},"value":2},null,null,null],[null,null,{"position":{"x":3,"y":2},"value":4},{"position":{"x":3,"y":3},"value":4}]]},"score":8,"over":false,"won":false,"keepPlaying":false}'
 
 
-a = '{"grid":{"size":4,"cells":[[null,null,null,null],[null,null,null,null],[{"position":{"x":2,"y":0},"value":2},null,
-null,null],[null,null,{"position":{"x":3,"y":2},"value":4},{"position":{"x":3,"y":3},"value":4}]]},"score":8,"over":false,
-"won":false,"keepPlaying":false}'
-
-
-
-""" Extracts the information from the gameState string provided
-	by the local storage
-	Output: score - int value, that stores the current points
+def extractInfos(gameState):
+	""" 
+	
+	Extracts the information from the gameState string provided
+	by the local storage of the website
+	
+	Output: 
+	dictonary with different gamestate values
+			score - int value, that stores the current points
 			board - 4x4 array, which stores the numbers on the board
 			
-"""
-def extractInfos(gameState):
-	gameState = gameState[1:-1].split(",")
+	"""
+	gameStateDict = {}
+	gameState = gameState[26:-1].split("]]},")
+	
+	# Extract grid
+	gridString = gameState[0].replace("]","")
+	gridString = gridString.replace("[","")
+	gridString = gridString.split(",")
+	
+	grid = np.zeros((4,4))
+	counter = 0
+	for i in len(gridString):
+		if "null" in gridString[i]:
+			counter += 1
+			continue
+		elif "position" in gridString[i]:
+			puffer = gridString[i+2]
+			grid[counter//4, counter%4] = int(puffer[8:-1])
+			counter += 1
+			
+	gameStateDict["grid"] = grid
+	
+	# Extract other informations
+	gridString = gameState[1].split(",")
+	gameStateDict["score"] = int(gridString[0][8:])
+	#gameStateDict["over"] = 
+	
+	return gameStateDict
 	
 	
-	
-print(extractInfos(a))
+print(extractInfos(b))
 
 
 driver = webdriver.Firefox()
@@ -36,7 +70,7 @@ for i in range(1):
 	elem.send_keys(Keys.ARROW_RIGHT)
 	
 result = driver.execute_script("return localStorage.getItem('gameState')")
-#print(driver.get("return javascript:localStorage.getItem('bestScore');"))
+print(driver.get("return localStorage.getItem('bestScore')"))
 #elem.send_keys(Keys.ARROW_DOWN)
 print(result.split(","))
 #print(driver.find_element_by_class_name("score-container").text)
