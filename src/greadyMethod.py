0715e15665c991591adbc736b34044440807a509
Fritjof Wolf <fritjofwolf@gmail.com>
2016-03-30 01:33:02 +0200

    gready method started

---
+++ src/greadyMethod.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def computeReward(grid, move):
+	"""
+	Simulates the given move on the given grid and returns the
+	expected reward.
+	"""
+	reward = 0
+	tmp = -1
+	if move == 0:
+		for i in range(0,4):
+			for j in range(0,4):
+				
+	elif move == 1:
+		elem.send_keys(Keys.ARROW_RIGHT)
+	elif move == 2:
+		elem.send_keys(Keys.ARROW_DOWN)
+	else:
+		elem.send_keys(Keys.ARROW_LEFT)
+	return reward
