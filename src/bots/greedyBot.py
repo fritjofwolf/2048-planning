1eb5c250a0bb31df5c91bdd4d847d53233058a98
Fritjof Wolf <fritjofwolf@gmail.com>
2017-08-19 14:24:08 +0200

    deleted dublicate method

--- src/bots/greedyBot.py
+++ src/bots/greedyBot.py
@@ -10,55 +10,28 @@ class GreedyBot(BaseBot):
 		"""
 		self._depth = depth
 	
-	def computeReward(self, grid, move):
-		"""
-		Simulates the given move on the given grid and returns the
-		expected reward.
-		"""
-		reward = 0
-		tmp = -1
-		if move == 0:
-			for i in range(0,4):
-				for j in range(0,4):
-					pass
-		elif move == 1:
-			elem.send_keys(Keys.ARROW_RIGHT)
-		elif move == 2:
-			elem.send_keys(Keys.ARROW_DOWN)
-		else:
-			elem.send_keys(Keys.ARROW_LEFT)
-		return reward
+	# def computeReward(self, grid, move):
+	# 	"""
+	# 	Simulates the given move on the given grid and returns the
+	# 	expected reward.
+	# 	"""
+	# 	reward = 0
+	# 	tmp = -1
+	# 	if move == 0:
+	# 		for i in range(0,4):
+	# 			for j in range(0,4):
+	# 				pass
+	# 	elif move == 1:
+	# 		elem.send_keys(Keys.ARROW_RIGHT)
+	# 	elif move == 2:
+	# 		elem.send_keys(Keys.ARROW_DOWN)
+	# 	else:
+	# 		elem.send_keys(Keys.ARROW_LEFT)
+	# 	return reward
 		
-	def selectMove(self, grid):
-		move = np.random.randint(4)
-		reward = 0
-		for i in range(4):
-			a = computeReward(grid, i)
-			if a > reward:
-				reward = a
-				move = i
-		return move
+	
 			
-	def computeReward(self, grid, move):
-		"""
-		Simulates the given move on the given grid and returns the
-		expected reward.
-		"""
-		reward = 0
-		tmp = -1
-		if move == 0:
-			for i in range(4):
-				reward += computeRowReward(grid[:,i])
-		elif move == 1:
-			for i in range(4):
-				reward += computeRowReward(grid[i,::-1])
-		elif move == 2:
-			for i in range(4):
-				reward += computeRowReward(grid[::-1,i])
-		else:
-			for i in range(4):
-				reward += computeRowReward(grid[i,:])
-		return reward
+	
 
 	def computeRowReward(self, row):
 		"""
@@ -82,5 +55,37 @@ class GreedyBot(BaseBot):
 			reward += 2*row2[1]
 		elif (len(row2) == 4 and row2[2] == row2[3]):
 			reward += 2*row2[2]
+		while len(row2) < 4:
+			row2.append(0)
 		return reward
 
+	def computeReward(self, grid, move):
+		"""
+		Simulates the given move on the given grid and returns the
+		expected reward.
+		"""
+		reward = 0
+		tmp = -1
+		if move == 0:
+			for i in range(4):
+				reward += self.computeRowReward(grid[:,i])
+		elif move == 1:
+			for i in range(4):
+				reward += self.computeRowReward(grid[i,::-1])
+		elif move == 2:
+			for i in range(4):
+				reward += self.computeRowReward(grid[::-1,i])
+		else:
+			for i in range(4):
+				reward += self.computeRowReward(grid[i,:])
+		return reward
+
+	def selectMove(self, grid):
+		move = np.random.randint(4)
+		reward = 0
+		for i in range(4):
+			a = self.computeReward(grid, i)
+			if a > reward:
+				reward = a
+				move = i
+		return move
