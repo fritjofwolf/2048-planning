b2697195501f9c7a55c892484ff64e9f28b2ffb4
Fritjof Wolf <fritjofwolf@gmail.com>
2018-10-21 10:42:15 +0200

    Finished offline game engine with tests

--- src/io2048/io_offline.py
+++ src/io2048/io_offline.py
@@ -17,11 +17,18 @@ class IOOffline:
         tmp_state, reward = self._make_move(action)
         if (tmp_state != self._board).any():
             next_state = self._add_new_tile(tmp_state)
-            done = self._check_for_finish()
+            done = self._is_done()
             self._update_state(next_state, reward, done)
             return next_state, reward, done
         else:
             return self._board, 0, self._done
+
+    def _is_done(self):
+        for i in range(4):
+            tmp_board, reward = self._make_move(i)
+            if (tmp_board != self._board).any():
+                return False
+        return True
 
     def _merge_row_cells_to_the_left(self, row):
         merged_row = np.zeros(4)
@@ -31,47 +38,53 @@ class IOOffline:
             merged_row[0] = 2*shifted_row[0]
             merged_row[1] = shifted_row[2]
             merged_row[2] = shifted_row[3]
+            reward = 2*shifted_row[0]
         elif shifted_row[0] == shifted_row[1] and shifted_row[2] == shifted_row[3]:
             merged_row[0] = 2*shifted_row[0]
             merged_row[1] = 2*shifted_row[2]
+            reward = 2*shifted_row[0] + 2*shifted_row[2]
         elif shifted_row[1] == shifted_row[2]:
             merged_row[0] = shifted_row[0]
             merged_row[1] = 2*shifted_row[1]
             merged_row[2] = shifted_row[3]
+            reward = 2*shifted_row[1]
+        elif shifted_row[2] == shifted_row[3]:
+            merged_row[0] = shifted_row[0]
+            merged_row[1] = shifted_row[1]
+            merged_row[2] = 2*shifted_row[2]
+            reward = 2*shifted_row[2]
         else:
             merged_row = shifted_row
-        return merged_row
+            reward = 0
+        return merged_row, reward
 
     def _update_state(self, next_state, reward, done):
         self._board = next_state
         self._score += reward
         self._done = done
 
-    
+    def _make_move(self, action):
+        if action == 0:
+            tmp_board, reward = self._compute_row_merges(self._board.T)
+            new_board = tmp_board.T
+        elif action == 1:
+            tmp_board, reward = self._compute_row_merges(self._board.T[::-1].T)
+            new_board = tmp_board.T[::-1].T
+        elif action == 2:
+            tmp_board, reward = self._compute_row_merges(self._board[::-1].T)
+            new_board = tmp_board.T[::-1]
+        else:
+            tmp_board, reward = self._compute_row_merges(self._board)
+            new_board = tmp_board
+        return new_board, reward
 
-    
-    # def _make_move(self, move):	
-    # 	"""
-    # 	Main method to handle the game logic
-
-    # 	Tries to make the given move, if not possible checks if there
-    # 	are legal moves left. In case the given move is legal, adds a new
-    # 	random tile to the grid.
-    # 	"""
-    # 	[grid,reward] = self.computeReward(move)
-    # 	if (self._gameState['grid'] == grid).all(): # illegal move
-    # 		for i in range(4):
-    # 			[grid,reward] = self.computeReward(i)
-    # 			if not (self._gameState['grid'] == grid).all():
-    # 				break
-    # 		else: # no legal move left
-    # 			self._gameState['over'] = True
-    # 			self._gameState['keepPlaying'] = False
-    # 	else: # legal move
-    # 		self._gameState['grid'] = grid
-    # 		self._gameState['score'] += reward
-    # 		self.addRandomTile()
-
+    def _compute_row_merges(self, board):
+        tmp_board = np.zeros((4,4))
+        reward = 0
+        for i in range(4):
+            tmp_board[i,:], row_reward = self._merge_row_cells_to_the_left(board[i,:])
+            reward += row_reward
+        return tmp_board, reward
 
     def _add_new_tile(self, board):
         empty_tiles = np.where(board == 0)
--- src/io2048/test_io_offline.py
+++ src/io2048/test_io_offline.py
@@ -19,22 +19,77 @@ def test_step():
 
 def test_merge_row_cells_to_the_left():
     io = IOOffline()
+
     input_row = np.array([1,1,2,2])
     expected_output = np.array([2,4,0,0])
-    assert((io._merge_row_cells_to_the_left(input_row) == expected_output).all())
+    expected_reward = 6
+    merged_row, reward = io._merge_row_cells_to_the_left(input_row)
+    assert((merged_row == expected_output).all())
+    assert(reward == expected_reward)
 
     input_row = np.array([1,1,1,1])
     expected_output = np.array([2,2,0,0])
-    assert((io._merge_row_cells_to_the_left(input_row) == expected_output).all())
+    expected_reward = 4
+    merged_row, reward = io._merge_row_cells_to_the_left(input_row)
+    assert((merged_row == expected_output).all())
+    assert(reward == expected_reward)
 
     input_row = np.array([0,0,0,2])
     expected_output = np.array([2,0,0,0])
-    assert((io._merge_row_cells_to_the_left(input_row) == expected_output).all())
+    expected_reward = 0
+    merged_row, reward = io._merge_row_cells_to_the_left(input_row)
+    assert((merged_row == expected_output).all())
+    assert(reward == expected_reward)
 
     input_row = np.array([3,1,1,2])
     expected_output = np.array([3,2,2,0])
-    assert((io._merge_row_cells_to_the_left(input_row) == expected_output).all())
+    expected_reward = 2
+    merged_row, reward = io._merge_row_cells_to_the_left(input_row)
+    assert((merged_row == expected_output).all())
+    assert(reward == expected_reward)
 
     input_row = np.array([1,2,3,4])
     expected_output = np.array([1,2,3,4])
-    assert((io._merge_row_cells_to_the_left(input_row) == expected_output).all())
+    expected_reward = 0
+    merged_row, reward = io._merge_row_cells_to_the_left(input_row)
+    assert((merged_row == expected_output).all())
+    assert(reward == expected_reward)
+
+    input_row = np.array([2,1,4,4])
+    expected_output = np.array([2,1,8,0])
+    expected_reward = 8
+    merged_row, reward = io._merge_row_cells_to_the_left(input_row)
+    assert((merged_row == expected_output).all())
+    assert(reward == expected_reward)
+
+
+def test_make_move():
+    io = IOOffline()
+    
+    io._board = np.array([[0,0,2,2],[4,2,8,2],[2,2,4,8],[16,2,2,8]])
+    expected_board = np.array([[4,0,0,0],[4,2,8,2],[4,4,8,0],[16,4,8,0]])
+    expected_reward = 12
+    new_board, reward = io._make_move(3)
+    assert((new_board == expected_board).all())
+    assert(reward == expected_reward)
+
+    io._board = np.array([[0,0,2,2],[4,2,8,2],[2,2,4,8],[16,2,2,8]])
+    expected_board = np.array([[0,0,2,0],[4,0,8,0],[2,2,4,4],[16,4,2,16]])
+    expected_reward = 24
+    new_board, reward = io._make_move(2)
+    assert((new_board == expected_board).all())
+    assert(reward == expected_reward)
+
+    io._board = np.array([[0,0,2,2],[4,2,8,2],[2,2,4,8],[16,2,2,8]])
+    expected_board = np.array([[0,0,0,4],[4,2,8,2],[0,4,4,8],[0,16,4,8]])
+    expected_reward = 12
+    new_board, reward = io._make_move(1)
+    assert((new_board == expected_board).all())
+    assert(reward == expected_reward)
+
+    io._board = np.array([[0,0,2,2],[4,2,8,2],[2,2,4,8],[16,2,2,8]])
+    expected_board = np.array([[4,4,2,4],[2,2,8,16],[16,0,4,0],[0,0,2,0]])
+    expected_reward = 24
+    new_board, reward = io._make_move(0)
+    assert((new_board == expected_board).all())
+    assert(reward == expected_reward)
