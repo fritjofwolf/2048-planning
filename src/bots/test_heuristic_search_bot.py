94f534d8e148db1f07cb46a4373548608ff25d87
Fritjof Wolf <fritjofwolf@gmail.com>
2018-10-27 19:41:04 +0200

    Additional testcase for depth of 1

--- src/bots/test_heuristic_search_bot.py
+++ src/bots/test_heuristic_search_bot.py
@@ -4,8 +4,10 @@ from bots.heuristic_search_bot import HeuristicSearchBot
 def test_compute_next_action_depth_1():
     bot = HeuristicSearchBot()
     state = np.array([[2,2,4,4],[0,2,0,4],[4,4,4,128],[2,4,0,128]])
-    expected_result = 2
     assert(bot.compute_next_action(state, 1) == 2, 284)
+
+    state = np.array([[8, 4, 0, 0],[4, 2, 0, 0],[4, 2, 0, 0],[8, 4, 0, 0]])
+    assert(bot.compute_next_action(state, 1) == 0, 12)
 
 def test_compute_next_action_depth_2():
     bot = HeuristicSearchBot()
