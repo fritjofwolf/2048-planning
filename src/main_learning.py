7afa907986cf422496b5232f9e753429f57f68ce
Fritjof Wolf <fritjofwolf@gmail.com>
2019-05-04 22:46:47 +0200

    Using learning script instead of jupyter notebook

--- src/main_learning.py
+++ src/main_learning.py
@@ -5,18 +5,11 @@ from io2048.io_offline import IOOffline
 from bots.random_bot import RandomBot
 from bots.rollout_bot import RolloutBot
 from rl_bots.ppo import PPO
+import matplotlib.pyplot as plt
+from scipy import polyfit
 
 if __name__ == '__main__':
-    # bot = RolloutBot(IOOffline(), RandomBot(), 1)
     ppo_agent = PPO()
-    ppo_agent.train(100)
-    # iterations = 100
-    # n_processors = 4
-    # cnt = 0
-    # total_time, average_score, average_time = evaluate_bot(iterations, n_processors)
-    # print('The total time was:', total_time)
-    # print('The average score per episode was:', average_score)
-    # print('The average time per episode was:', average_time)
-
+    results = ppo_agent.train(2*10**4)
 
 
